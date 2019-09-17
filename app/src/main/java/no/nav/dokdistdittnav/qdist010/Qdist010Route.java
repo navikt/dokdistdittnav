9f8bcfcce8074e6f494bed10ab42b2c60feb1103
Sigurd Midttun <sigurd.midttun@nav.no>
2019-09-17 15:43:16 +0200

    MMA-3448  Justerte på logging

--- app/src/main/java/no/nav/dokdistdittnav/qdist010/Qdist010Route.java
+++ app/src/main/java/no/nav/dokdistdittnav/qdist010/Qdist010Route.java
@@ -86,7 +86,7 @@ public class Qdist010Route extends SpringRouteBuilder {
 				.routePolicy(qdist010MetricsRoutePolicy)
 				.setExchangePattern(ExchangePattern.InOnly)
 				.process(new IdsProcessor())
-				.log(LoggingLevel.INFO, log, "qdist010 har mottatt forsendelse med " + getIdsForLogging())
+				.log(LoggingLevel.INFO, log, "qdist010 har mottatt forsendelse med forsendelseId=${exchangeProperty." + PROPERTY_FORSENDELSE_ID + "}")
 				.to("validator:no/nav/meldinger/virksomhet/dokdistfordeling/xsd/qdist008/out/distribuertilkanal.xsd")
 				.unmarshal(new JaxbDataFormat(JAXBContext.newInstance(DistribuerTilKanal.class)))
 				.bean(distribuerForsendelseTilDittNavMapper)
@@ -94,23 +94,20 @@ public class Qdist010Route extends SpringRouteBuilder {
 				.marshal(dokumentHenvendelseFormat)
 				.convertBodyTo(String.class, StandardCharsets.UTF_8.toString())
 				.to("jms:" + dokumentHenvendelse.getQueueName())
-				.log(LoggingLevel.INFO, log, "qdist010 har sendt dokumenthenvendelse for forsendelse med " + getIdsForLogging() + "og" + getAdditionalIdsForLogging())
+				.log(LoggingLevel.INFO, log, "qdist010 har sendt dokumenthenvendelse for forsendelse med " + getIdsForLogging())
 				.setBody(exchangeProperty(Qdist010Service.PROPERTY_UNMARSHALLED_VARSEL))
 				.marshal(varselFormat)
 				.convertBodyTo(String.class, StandardCharsets.UTF_8.toString())
 				.to("jms:" + varselUtsending.getQueueName())
 				.bean(dokdistStatusUpdater)
-				.log(LoggingLevel.INFO, log, "qdist010 har sendt varsel og oppdatert status=EKSPEDERT i dokdistDb for forsendelse med " + getIdsForLogging() + "og" + getAdditionalIdsForLogging());
+				.log(LoggingLevel.INFO, log, "qdist010 har sendt varsel og oppdatert status=EKSPEDERT i dokdistDb for forsendelse med " + getIdsForLogging());
 	}
 
 	public static String getIdsForLogging() {
-		return "bestillingsId=${exchangeProperty." + PROPERTY_BESTILLINGS_ID + "} og " +
-				"forsendelseId=${exchangeProperty." + PROPERTY_FORSENDELSE_ID + "}";
-	}
-
-	public static String getAdditionalIdsForLogging() {
-		return "journalpostId=${exchangeProperty." + PROPERTY_JOURNALPOST_ID + "} og " +
-				"varselbestillingId==${exchangeProperty." + PROPERTY_VARSELBESTILLING_ID + "}";
+		return "bestillingsId=${exchangeProperty." + PROPERTY_BESTILLINGS_ID + "}, " +
+				"forsendelseId=${exchangeProperty." + PROPERTY_FORSENDELSE_ID + "}, " +
+				"journalpostId=${exchangeProperty." + PROPERTY_JOURNALPOST_ID + "} og " +
+				"varselbestillingId=${exchangeProperty." + PROPERTY_VARSELBESTILLING_ID + "}";
 	}
 
 	public DataFormat setupVarselFormat() {
