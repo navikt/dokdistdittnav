a22f64ab459fe7e41fd41b6fe14e134c5602dae6
Christian Strandenæs <Christian.Fredrik.Osterby.Strandenes@nav.no>
2022-09-28 11:40:05 +0200

    MMA-6390 håndter exceptions på en reactive måte

--- core/src/main/java/no/nav/dokdistdittnav/consumer/dokarkiv/DokarkivConsumer.java
+++ core/src/main/java/no/nav/dokdistdittnav/consumer/dokarkiv/DokarkivConsumer.java
@@ -1,5 +1,7 @@
 package no.nav.dokdistdittnav.consumer.dokarkiv;
 
+import io.netty.handler.timeout.ReadTimeoutException;
+import io.netty.handler.timeout.TimeoutException;
 import lombok.extern.slf4j.Slf4j;
 import no.nav.dokdistdittnav.config.properties.DokdistdittnavProperties;
 import no.nav.dokdistdittnav.exception.functional.AbstractDokdistdittnavFunctionalException;
@@ -7,17 +9,17 @@ import no.nav.dokdistdittnav.exception.technical.AbstractDokdistdittnavTechnical
 import no.nav.dokdistdittnav.metrics.Monitor;
 import org.slf4j.MDC;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.retry.annotation.Backoff;
 import org.springframework.retry.annotation.Retryable;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
+import org.springframework.web.reactive.function.client.ClientResponse;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriBuilder;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
+import reactor.core.publisher.Mono;
 
-import java.net.URI;
-import java.util.function.Function;
+import java.util.function.Consumer;
 
 import static java.lang.String.format;
 import static no.nav.dokdistdittnav.constants.MdcConstants.CALL_ID;
@@ -42,22 +44,32 @@ public class DokarkivConsumer {
 	@Retryable(include = AbstractDokdistdittnavTechnicalException.class, backoff = @Backoff(delay = DELAY_SHORT, multiplier = MAX_ATTEMPTS_SHORT))
 	@Monitor(value = DOKARKIV_CONSUMER, extraTags = {PROCESS, "oppdaterDistribusjonsinfo"}, histogram = true)
 	public void settTidLestHoveddokument(JournalPostId journalPostId, OppdaterDistribusjonsInfo feilregistrerForsendelse) {
+		webClient.patch()
+				.uri(dokdistdittnavProperties.getDokarkiv().getOppdaterDistribusjonsinfoURI(journalPostId))
+				.headers(this::createHeaders)
+				.bodyValue(feilregistrerForsendelse)
+				.retrieve()
+				.bodyToMono(String.class)
+				.doOnError(handleError(journalPostId))
+				.block();
+	}
 
-		try {
-			webClient.patch()
-					.uri(dokdistdittnavProperties.getDokarkiv().getOppdaterDistribusjonsinfoURI(journalPostId))
-					.headers(this::createHeaders)
-					.bodyValue(feilregistrerForsendelse)
-					.retrieve();
-		} catch (HttpClientErrorException e) {
-			log.error("Kall mot dokarkiv feilet funksjonelt ved registrering av lest status for journalpost med id={}, feilmelding={}", journalPostId.value(), e.getMessage());
-			throw new AbstractDokdistdittnavFunctionalException(format("Kall mot dokarkiv feilet ved registrering av lest status for journalpost med id=%s, feilmelding=%s", journalPostId.value(), e.getMessage()), e) {
-			};
-		} catch (HttpServerErrorException e) {
-			log.error("Kall mot dokarkiv feilet funksjonelt ved registrering av lest status for journalpost med id={}, feilmelding={}", journalPostId.value(), e.getMessage());
-			throw new AbstractDokdistdittnavTechnicalException(format("Kall mot dokarkiv feilet ved registrering av lest status for journalpost med id=%s, feilmelding=%s", journalPostId.value(), e.getMessage()), e) {
-			};
-		}
+	private Consumer<Throwable> handleError(JournalPostId journalPostId) {
+		return error -> {
+			if (error instanceof WebClientResponseException response && ((WebClientResponseException) error).getStatusCode().is4xxClientError()) {
+				log.error("Kall mot dokarkiv feilet funksjonelt ved registrering av lest status for journalpost med id={}, feilmelding={}", journalPostId.value(), error.getMessage());
+				throw new DokarkivOppdaterDistribusjonsinfoFunctionalException(
+						String.format("Kall mot SAF (GraphQL) feilet med status=%s, feilmelding=%s",
+								response.getRawStatusCode(),
+								response.getMessage()),
+						error);
+			} else {
+				log.error("Kall mot dokarkiv feilet teknisk ved registrering av lest status for journalpost med id={}, feilmelding={}", journalPostId.value(), error.getMessage());
+				throw new DokarkivOppdaterDistribusjonsinfoTechnicalException(
+						String.format("Kall mot SAF (GraphQL) feilet med feilmelding=%s", error.getMessage()),
+						error);
+			}
+		};
 	}
 
 	private void createHeaders(HttpHeaders headers) {
