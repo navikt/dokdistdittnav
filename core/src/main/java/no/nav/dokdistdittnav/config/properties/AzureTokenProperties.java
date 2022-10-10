1187d111e753bb44256c3655f3e39abcf7fc7be9
Anders Opskar Voldsund <anders.voldsund@nav.no>
2022-10-10 14:36:16 +0200

    Test å bruke Authentication i OAuth2AuthorizeRequest
    Co-authored-by: Joakim Borgersen <joakim.borgersen@nav.no>

--- core/src/main/java/no/nav/dokdistdittnav/config/properties/AzureTokenProperties.java
+++ core/src/main/java/no/nav/dokdistdittnav/config/properties/AzureTokenProperties.java
@@ -1,10 +1,14 @@
 package no.nav.dokdistdittnav.config.properties;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.constraints.NotEmpty;
+
+import static org.springframework.security.core.authority.AuthorityUtils.createAuthorityList;
 
 @Validated
 @ConfigurationProperties(prefix = "azure.app")
@@ -15,13 +19,14 @@ public record AzureTokenProperties(
 		@NotEmpty String tenantId,
 		@NotEmpty String wellKnownUrl
 ) {
-	public static final String SPRING_DEFAULT_PRINCIPAL = "anonymousUser";
+	public static final Authentication ANONYMOUS_AUTHENTICATION =  new AnonymousAuthenticationToken(
+			"anonymous", "anonymousUser", createAuthorityList("ROLE_ANONYMOUS"));
 	public static final String CLIENT_REGISTRATION_ID = "azure";
 
 	public static OAuth2AuthorizeRequest getOAuth2AuthorizeRequestForAzure() {
 		return OAuth2AuthorizeRequest
 				.withClientRegistrationId(CLIENT_REGISTRATION_ID)
-				.principal(SPRING_DEFAULT_PRINCIPAL)
+				.principal(ANONYMOUS_AUTHENTICATION)
 				.build();
 	}
 }
