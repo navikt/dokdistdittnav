b35eeca936745e6dbc19bcb6f54d8b18bc750c80
tsigab <tsigab.angosom@gmail.com>
2023-02-22 14:22:04 +0100

    MMA-6711 la til varslingstidspunkt felt.

--- core/src/main/java/no/nav/dokdistdittnav/consumer/dokumentdistribusjon/Notifikasjon.java
+++ core/src/main/java/no/nav/dokdistdittnav/consumer/dokumentdistribusjon/Notifikasjon.java
@@ -1,9 +1,12 @@
 package no.nav.dokdistdittnav.consumer.dokumentdistribusjon;
 
+import java.time.LocalDateTime;
+
 public record Notifikasjon(
 		String kanal,
 		String tittel,
 		String tekst,
-		String kontaktInfo
+		String kontaktInfo,
+		LocalDateTime varslingstidspunkt
 ) {
 }
