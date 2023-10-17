db9ac2832db6a65e55d33b51e8a4ae0c1b1e37f3
Anders Opskar Voldsund <anders.voldsund@nav.no>
2023-10-17 11:08:56 +0200

    MMA-7106 Rydd opp i korleis tekniske feil blir handtert av Camel
    Det er konfig. for å handtere dette manuelt, men Camel hadde også oppsett for dette, som gjorde at dei gjekk i beina på kvarandre. Dette resulterte i uynskja reset av offset ved tekniske feil.
    
    Co-authored-by: Christian Tumyr <christian.tumyr@nav.no>

--- kdist002/src/main/java/no/nav/dokdistdittnav/kdist002/Kdist002Route.java
+++ kdist002/src/main/java/no/nav/dokdistdittnav/kdist002/Kdist002Route.java
@@ -66,10 +66,10 @@ public class Kdist002Route extends RouteBuilder {
 					Throwable exception = exchange.getProperty(EXCEPTION_CAUGHT, Throwable.class);
 					if (exception != null) {
 						DefaultKafkaManualCommit manual = exchange.getIn().getHeader(MANUAL_COMMIT, DefaultKafkaManualCommit.class);
-						manual.getConsumer().seek(manual.getPartition(), manual.getRecordOffset());
+						manual.getCamelExchangePayload().consumer.seek(manual.getPartition(), manual.getRecordOffset());
 
-						log.error("Kdist002 Teknisk feil. Seek tilbake til record(topic={}, partition={}, offset={})", manual.getTopicName(),
-								manual.getPartition().partition(), manual.getRecordOffset());
+						log.error("Kdist002 Teknisk feil. Seek tilbake til record(topic={}, partition={}, offset={})",
+								manual.getTopicName(), manual.getPartition().partition(), manual.getRecordOffset());
 					}
 				})
 				.retryAttemptedLogLevel(ERROR)
