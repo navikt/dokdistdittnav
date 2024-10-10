c311f24013999255a61be24ad7d3bdf35134b6af
Joakim Bjørnstad <joakim.bjornstad@nav.no>
2024-10-10 14:05:33 +0200

    MMA-7642 Fjernet srvdokdistdittnavcert, kun TLS
     * Bruker kun secure channel
     * Alltid TLS på JMS/MQ koblinger

--- export-vault-secrets.sh
+++ export-vault-secrets.sh
@@ -11,18 +11,3 @@ then
     echo "Setting SERVICEUSER_PASSWORD"
     export SERVICEUSER_PASSWORD=$(cat /var/run/secrets/nais.io/srvdokdistdittnav/password)
 fi
-
-if test -f /var/run/secrets/nais.io/vault/certificate_keystore
-then
-    echo "Setting SRVDOKDISTDITTNAVCERT_KEYSTORE"
-    CERT_PATH='/var/run/secrets/nais.io/vault/certificate_keystore-extracted'
-    openssl base64 -d -A -in /var/run/secrets/nais.io/vault/certificate_keystore -out $CERT_PATH
-    export SRVDOKDISTDITTNAVCERT_KEYSTORE=$CERT_PATH
-fi
-
-if test -f /var/run/secrets/nais.io/vault/certificate_keystore_password
-then
-    echo "Setting SRVDOKDISTDITTNAVCERT_PASSWORD"
-    export SRVDOKDISTDITTNAVCERT_PASSWORD=$(cat /var/run/secrets/nais.io/vault/certificate_keystore_password)
-fi
-
